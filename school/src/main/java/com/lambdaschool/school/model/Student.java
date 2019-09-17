0c4dad5fbeffd04ecfdf89a8fce972520322f045
antilou86 <antilou86@gmail.com>
2019-09-17 19:05:22 -0400

    MVP complete. updated models with swagger annotations.

--- school/src/main/java/com/lambdaschool/school/model/Student.java
+++ school/src/main/java/com/lambdaschool/school/model/Student.java
@@ -1,27 +1,34 @@
 package com.lambdaschool.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+@ApiModel(value = "Student", description = "the Student Entity")
 @Entity
 @Table(name = "student")
 public class Student
 {
+    @ApiModelProperty(name = "studid", value = "primary key for Student", required = true, example= "1")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long studid;
 
+    @ApiModelProperty(name = "student name", value = "students actual name", required = true, example="John Smith")
     private String studname;
+
 
     @ManyToMany
     @JoinTable(name = "studcourses",
                joinColumns = {@JoinColumn(name = "studid")},
                inverseJoinColumns = {@JoinColumn(name = "courseid")})
     @JsonIgnoreProperties("students")
+    @ApiModelProperty(name = "course list", value="list of courses for this student", example = "[]")
     private List<Course> courses = new ArrayList<>();
 
     public Student()
